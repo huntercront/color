bf32b52e9f83e566d2c0b126761284cd9c4baf07
Semen <huntercront@gmail.com>
2018-11-13 22:45:00 +0300

    test

---
+++ js/mail.js
@@ -0,0 +1,12 @@
+var red = document.getElementById("red");
+var green = document.getElementById("green");
+var royalblue = document.getElementById("royalblue");
+	red.onclick= function() {
+		var btn = document.getElementById("change-color").style.backgroundColor='red';
+			}
+			green.onclick= function() {
+				var btn = document.getElementById("change-color").style.backgroundColor='green';
+					}
+					royalblue.onclick= function() {
+						var btn = document.getElementById("change-color").style.backgroundColor='royalblue';
+							}
