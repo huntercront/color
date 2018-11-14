8d7c72a0b1ecae13965928811bb01e886ad38f30
huntercront <38252870+huntercront@users.noreply.github.com>
2018-11-14 12:17:39 +0300

    adaptiv+

--- js/mail.js
+++ js/mail.js
@@ -10,3 +10,15 @@ var royalblue = document.getElementById("royalblue");
 					royalblue.onclick= function() {
 						var btn = document.getElementById("change-color").style.backgroundColor='royalblue';
 							}
+							var redframe = document.getElementById("redframe");
+							var greenframe = document.getElementById("greenframe");
+							var royalblueframe = document.getElementById("royalblueframe");
+							redframe.onclick= function() {
+								var btnframe = document.getElementById("change-color-frame").style.backgroundColor='red';
+									}
+									greenframe.onclick= function() {
+										var btnframe = document.getElementById("change-color-frame").style.backgroundColor='green';
+											}
+											royalblueframe.onclick= function() {
+												var btnframe = document.getElementById("change-color-frame").style.backgroundColor='royalblue';
+													}
